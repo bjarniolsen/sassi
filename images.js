ffaea6e73ad4f5ae3f18b45eee21b7d7ead71be0
bjarniolsen <bjarniolsen@8ff12202-8362-42a9-a22f-265cad64ddee>
2014-01-15 21:44:07 +0000

    Multible image upload is ready and you can add a category to the batch.
    git-svn-id: https://svn.bjarniolsen.dk/node/sassi@404 8ff12202-8362-42a9-a22f-265cad64ddee

--- images.js
+++ images.js
@@ -96,98 +96,104 @@ exports.addImage = function(req, res) {
 }
 
 exports.addImages = function(req, res) {
-	// TODO: Something is wrong with target_path and the other paths i think
-	var newId = (images.length > 0) ? (images[images.length-1].id + 1) : 0;
-    for(var image in req.files.images) {
-		//console.log(req.files.images[image].path);
-		if (req.files.images.hasOwnProperty(image)) {
-			var newJson =  {
-					"id": newId,
-					"imageurl": req.files.images[image].name,
-					"title": "**temp",
-			};
-			// get the temporary location of the file
-			var tmp_path = req.files.images[image].path;
-			// set where the file should actually exists - in this case it is in the "images" directory
-			var target_path = __dirname + '/assets/images/orig/' + req.files.images[image].name;
-			var image_large = __dirname + '/assets/images/large/' + req.files.images[image].name;
-			var thumb_large = __dirname + '/assets/images/thumb/large/' + req.files.images[image].name;
-			var thumb_medium = __dirname + '/assets/images/thumb/medium/' + req.files.images[image].name;
+	if(req.files.images.length) {
 
-			//console.log(tmp_path, target_path, thumb_large, thumb_medium);
+		var newId = (images.length > 0) ? (images[images.length-1].id + 1) : 0;
+		var category = req.body.newcategory ? req.body.newcategory : req.body.category; 
 
-			// move the file from the temporary location to the intended location
-			upload(tmp_path, target_path, thumb_large, thumb_medium);
-    		// Append new json to images object
-    		images.push(newJson);
-    		fs.writeFile(file, JSON.stringify(images, null, 4), function(err) {
-    			if (err) {
-    				console.log(err);
-    			} else {
-    				console.log("yay");
-    			}
-    		});
+    	for(var image in req.files.images) {
+			//console.log(req.files.images[image].path);
+			if (req.files.images.hasOwnProperty(image)) {
+				var newJson =  {
+						"id": newId,
+						"category": category,
+						"imageurl": req.files.images[image].name,
+						"title": "**temp",
+				};
+				// get the temporary location of the file
+				var tmp_path = req.files.images[image].path;
+				// set where the file should actually exists - in this case it is in the "images" directory
+				var target_path = __dirname + '/assets/images/orig/' + req.files.images[image].name;
+				var image_large = __dirname + '/assets/images/large/' + req.files.images[image].name;
+				var thumb_large = __dirname + '/assets/images/thumb/large/' + req.files.images[image].name;
+				var thumb_medium = __dirname + '/assets/images/thumb/medium/' + req.files.images[image].name;
+
+				//console.log(tmp_path, target_path, thumb_large, thumb_medium);
+
+				// move the file from the temporary location to the intended location
+				upload(tmp_path, target_path, image_large, thumb_large, thumb_medium);
+    			// Append new json to images object
+    			images.push(newJson);
+    			fs.writeFile(file, JSON.stringify(images, null, 4), function(err) {
+    				if (err) {
+    					console.log(err);
+    				} else {
+    					console.log("yay");
+    				}
+    			});
+    		}
+			newId++;
     	}
-		newId++;
-    }
 
-    function upload(tmp, target, thumb_l, thumb_m) {
-    	console.log("upload called: ", tmp, target, thumb_l, thumb_m);
-    	// TODO: these urls are OK. But im.resize fucks it up... 
-		/*im.resize({
-			srcPath: target,
-			dstPath: thumb_l,
-			width: 200,
-			height: "200^",
-			customArgs: [
-				"-gravity", "center",
-				"-extent", "200x200"
-			]
-		}, function(err, stdout, stderr) {
-			if (err) throw err;
-			console.log("resized image to fit within 200x200px", thumb_l);
-
-		});
-		im.resize({
-			srcPath: target,
-			dstPath: thumb_m,
-			width: 80,
-			height: "80^",
-			customArgs: [
-				"-gravity", "center",
-				"-extent", "80x80"
-			]
-		}, function(err, stdout, stderr) {
-			if (err) throw err;
-			console.log("resized image to fit within 80x80px", thumb_m);
-		});*/
-		fs.rename(tmp, target, function(err) {
-			if (err) throw err;
-			/*im.identify(target_path, function(err, features) {
+    	function upload(tmp, target, image_l, thumb_l, thumb_m) {
+			fs.rename(tmp, target, function(err) {
 				if (err) throw err;
-				console.log(features.width, features.height);
-				if(features.width > 700) {
+
+				im.resize({
+					srcPath: target,
+					dstPath: thumb_l,
+					width: 200,
+					height: "200^",
+					customArgs: [
+						"-gravity", "center",
+						"-extent", "200x200"
+					]
+				}, function(err, stdout, stderr) {
+					if (err) throw err;
+					console.log("resized image to fit within 200x200px", thumb_l);
+
 					im.resize({
-						srcPath: target_path,
-						dstPath: image_large,
-						width: 700,
+						srcPath: target,
+						dstPath: thumb_m,
+						width: 80,
+						height: "80^",
+						customArgs: [
+							"-gravity", "center",
+							"-extent", "80x80"
+						]
 					}, function(err, stdout, stderr) {
 						if (err) throw err;
-						console.log("resized image to width 700px");
+						console.log("resized image to fit within 80x80px", thumb_m);
+
+						im.identify(target, function(err, features) {
+							if (err) throw err;
+							console.log(features.width, features.height);
+							if(features.width > 700) {
+								im.resize({
+									srcPath: target,
+									dstPath: image_l,
+									width: 700,
+								}, function(err, stdout, stderr) {
+									if (err) throw err;
+									console.log("resized image to width 700px");
+								});
+							} else {
+								ncp(target, image_l, function(err) {
+									if (err) throw err;
+									console.log("original image copied to /large dir"); 
+								});
+							}
+						});
+						// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+						fs.unlink(tmp_path, function() {
+							if (err) throw err;
+							console.log('File uploaded to: ' + target_path + ' - ?? bytes');
+						});
 					});
-				} else {
-					ncp(target_path, image_large, function(err) {
-						if (err) throw err;
-						console.log("original image copied to /large dir"); 
-					});
-				}
-			});*/
-			// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-			/*fs.unlink(tmp_path, function() {
-				if (err) throw err;
-				console.log('File uploaded to: ' + target_path + ' - ?? bytes');
-			});*/
-		});
+
+				});
+			});
+    	}
     }
 }
 
