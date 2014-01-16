fa00cd5f99b8f1bfcf044e538611fbcdc4ba9c03
bjarniolsen <bjarniolsen@8ff12202-8362-42a9-a22f-265cad64ddee>
2014-01-16 09:01:20 +0000

    Added find, cookie and hasClass methods to $.js lib.
    git-svn-id: https://svn.bjarniolsen.dk/node/sassi@405 8ff12202-8362-42a9-a22f-265cad64ddee

--- assets/js/lib/min.js/src/$.js
+++ assets/js/lib/min.js/src/$.js
@@ -1,69 +1,87 @@
 /*globals Node:true, NodeList:true*/
 $ = (function (document, window, $) {
-  // Node covers all elements, but also the document objects
-  var node = Node.prototype,
-      nodeList = NodeList.prototype,
-      forEach = 'forEach',
-      trigger = 'trigger',
-      each = [][forEach],
-      // note: createElement requires a string in Firefox
-      dummy = document.createElement('i');
+	// Node covers all elements, but also the document objects
+	var node = Node.prototype,
+		nodeList = NodeList.prototype,
+		forEach = 'forEach',
+		trigger = 'trigger',
+		each = [][forEach],
+		cookies = {},
+		// note: createElement requires a string in Firefox
+		dummy = document.createElement('i');
 
-  nodeList[forEach] = each;
+	nodeList[forEach] = each;
 
-  // we have to explicitly add a window.on as it's not included
-  // in the Node object.
-  window.on = node.on = function (event, fn) {
-    this.addEventListener(event, fn, false);
+	// we have to explicitly add a window.on as it's not included
+	// in the Node object.
+	window.on = node.on = function (event, fn) {
+		this.addEventListener(event, fn, false);
 
-    // allow for chaining
-    return this;
-  };
+		// allow for chaining
+		return this;
+	};
 
-  nodeList.on = function (event, fn) {
-    this[forEach](function (el) {
-      el.on(event, fn);
-    });
-    return this;
-  };
+	nodeList.on = function (event, fn) {
+		this[forEach](function (el) {
+			el.on(event, fn);
+		});
+		return this;
+	};
 
-  // we save a few bytes (but none really in compression)
-  // by using [trigger] - really it's for consistency in the
-  // source code.
-  window[trigger] = node[trigger] = function (type, data) {
-    // construct an HTML event. This could have
-    // been a real custom event
-    var event = document.createEvent('HTMLEvents');
-    event.initEvent(type, true, true);
-    event.data = data || {};
-    event.eventName = type;
-    event.target = this;
-    this.dispatchEvent(event);
-    return this;
-  };
+	// we save a few bytes (but none really in compression)
+	// by using [trigger] - really it's for consistency in the
+	// source code.
+	window[trigger] = node[trigger] = function (type, data) {
+		// construct an HTML event. This could have
+		// been a real custom event
+		var event = document.createEvent('HTMLEvents');
+		event.initEvent(type, true, true);
+		event.data = data || {};
+		event.eventName = type;
+		event.target = this;
+		this.dispatchEvent(event);
+		return this;
+	};
 
-  nodeList[trigger] = function (event) {
-    this[forEach](function (el) {
-      el[trigger](event);
-    });
-    return this;
-  };
+	nodeList[trigger] = function (event) {
+		this[forEach](function (el) {
+			el[trigger](event);
+		});
+		return this;
+	};
 
-  $ = function (s) {
-    // querySelectorAll requires a string with a length
-    // otherwise it throws an exception
-    var r = document.querySelectorAll(s || '☺'),
-        length = r.length;
-    // if we have a single element, just return that.
-    // if there's no matched elements, return a nodeList to chain from
-    // else return the NodeList collection from qSA
-    return length == 1 ? r[0] : r;
-  };
 
-  // $.on and $.trigger allow for pub/sub type global
-  // custom events.
-  $.on = node.on.bind(dummy);
-  $[trigger] = node[trigger].bind(dummy);
+	node.find = function (klass) {
+		var r = this.querySelectorAll(klass || '☺'),
+			length = r.length;
+		return length == 1 ? r[0] : r;
+	};
 
-  return $;
+	node.hasClass = function (klass) {
+		return ((" " + this.className + " ").replace(/[\n\t]/g, " ").indexOf(klass) > -1) ? true : false;
+	}
+
+	// Get a cookie value
+	cookies.get = function(key) {
+		return unescape(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + escape(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+	}
+
+	$ = function (s) {
+		// querySelectorAll requires a string with a length
+		// otherwise it throws an exception
+		var r = document.querySelectorAll(s || '☺'),
+    		length = r.length;
+		// if we have a single element, just return that.
+		// if there's no matched elements, return a nodeList to chain from
+		// else return the NodeList collection from qSA
+		return length == 1 ? r[0] : r;
+	};
+
+	// $.on and $.trigger allow for pub/sub type global
+	// custom events.
+	$.on = node.on.bind(dummy);
+	$[trigger] = node[trigger].bind(dummy);
+	$.cookies = cookies;
+
+	return $;
 })(document, this);
